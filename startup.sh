b1fd8c8febd2cb8f374846f41dbd1ddf8ee434b6
Per Mosegaard <permosegaard@googlemail.com>
2016-05-11 02:22:15 +0100

    Update startup.sh
    minor fixes

--- startup.sh
+++ startup.sh
@@ -2,16 +2,4 @@
 
 ip route change default via 172.17.42.254
 
-if [ -f /opt/teamspeak3-server_linux_amd64/ts3server.ini ]
-then
-	/opt/teamspeak3-server_linux_amd64/ts3server_minimal_runscript.sh inifile="/opt/teamspeak3-server_linux_amd64/ts3server.ini"
-else
-	echo -e "208.167.241.190\n208.167.241.185\n208.167.241.186\n108.61.78.147\n108.61.78.148\n108.61.78.149\n108.61.78.150" > /opt/teamspeak3-server_linux_amd64/query_ip_whitelist.txt
-	/opt/teamspeak3-server_linux_amd64/ts3server_minimal_runscript.sh \
-		query_ip_whitelist="/opt/teamspeak3-server_linux_amd64/query_ip_whitelist.txt" \
-		query_ip_backlist="/opt/teamspeak3-server_linux_amd64/query_ip_blacklist.txt" \
-		logpath="/opt/teamspeak3-server_linux_amd64/logs/" \
-		licensepath="/opt/teamspeak3-server_linux_amd64/" \
-		inifile="/opt/teamspeak3-server_linux_amd64/ts3server.ini" \
-		createinifile=1 
-fi
+/opt/teamspeak3-server_linux_amd64/ts3server_minimal_runscript.sh inifile="/opt/teamspeak3-server_linux_amd64/ts3server.ini"
