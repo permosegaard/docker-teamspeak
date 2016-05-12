59a9bffdb8df74a8b9d243bb56aa8cdcf5075e14
Per Mosegaard <permosegaard@googlemail.com>
2016-05-12 12:05:45 +0100

    Update startup.sh
    minor fixes

--- startup.sh
+++ startup.sh
@@ -2,4 +2,4 @@
 
 ip route change default via 172.17.42.254
 
-/opt/teamspeak3-server_linux_amd64/ts3server_minimal_runscript.sh inifile="/opt/teamspeak3-server_linux_amd64/ts3server.ini"
+cd /server/ && ./ts3server_minimal_runscript.sh inifile=./ts3server.ini
