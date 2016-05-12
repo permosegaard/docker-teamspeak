d7c26783cfef16af480f0afb9b452df0662df8b1
Per Mosegaard <permosegaard@googlemail.com>
2016-05-12 22:53:29 +0100

    Update provision.sh
    minor changes

--- provision.sh
+++ provision.sh
@@ -7,38 +7,5 @@ apt-get install -qy wget bzip2
 mkdir /server/
 
 wget -qO- http://dl.4players.de/ts/releases/3.0.12.3/teamspeak3-server_linux_amd64-3.0.12.3.tar.bz2 | tar -xj --strip-components 1 -C /server
-env > /environment1.txt
-tee /server/ts3server.ini <<EOF
-machine_id=
-default_voice_port=${PORT_9987}
-voice_ip=0.0.0.0
-licensepath=
-filetransfer_port=${PORT_30033}
-filetransfer_ip=0.0.0.0
-query_port=${PORT_10011}
-query_ip=0.0.0.0
-query_ip_whitelist=whitelist.txt
-query_ip_blacklist=blacklist.txt
-dbplugin=ts3db_sqlite3
-dbpluginparameter=
-dbsqlpath=sql/
-dbsqlcreatepath=create_sqlite/
-dbconnections=10
-logpath=logs/
-logquerycommands=0
-dbclientkeepdays=30
-logappend=0
-query_skipbruteforcecheck=0
-EOF
-
-tee /server/whitelist.txt <<EOF
-208.167.241.190
-208.167.241.185
-208.167.241.186
-108.61.78.147
-108.61.78.148
-108.61.78.149
-108.61.78.150
-EOF
 
 apt-get clean && rm -Rf /var/lib/apt/lists/*
