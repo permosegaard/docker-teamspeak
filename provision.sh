a182cf55ce03fe2d92c1d51faf4c76b7d20a09d5
Per Mosegaard <permosegaard@googlemail.com>
2016-05-30 15:52:06 +0100

    Update provision.sh
    minor update

--- provision.sh
+++ provision.sh
@@ -5,10 +5,6 @@ export DEBIAN_FRONTEND=noninteractive
 echo "Europe/London" > /etc/timezone && dpkg-reconfigure tzdata
 apt-get update && apt-get install -qy iproute2
 
-apt-get install -qy wget bzip2
-
-mkdir /server/
-
-wget -qO- http://dl.4players.de/ts/releases/3.0.12.3/teamspeak3-server_linux_amd64-3.0.12.3.tar.bz2 | tar -xj --strip-components 1 -C /server
+apt-get install -qy aufs-tools wget bzip2
 
 apt-get clean && rm -Rf /var/lib/apt/lists/*
